7906cbd375ca4aa0fed0770e847bb9f34f571daa
Anikin Luke Abales <47096239+ph-fox@users.noreply.github.com>
2023-02-18 16:09:53 +0800

    Create rpt-merchant-sales.php

---
+++ views/admin/rpt-merchant-sales.php
@@ -0,0 +1,93 @@
+<div class="spacer"></div>
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                
+<form id="frm_table_list" method="POST" class="report uk-form uk-form-horizontal" >
+<?php echo CHtml::hiddenField('start_date',isset($_GET['start_date'])?$_GET['start_date']:"")?>
+<?php echo CHtml::hiddenField('end_date',isset($_GET['end_date'])?$_GET['end_date']:"")?>
+
+<?php 
+$order_stats=Yii::app()->functions->orderStatusList2(false);    
+?>
+
+<div class="row">
+  <label class="form-label"><?php echo Yii::t("default","Merchant Name")?></label>
+  <?php 
+  echo CHtml::dropDownList('merchant_id','',
+  (array)FunctionsV3::merchantList(true,true)
+  ,array(
+    'class'=>'p-1 border rounded',    
+  ))
+  ?>
+</div>
+  
+<div class="row">
+  <label class="form-label"><?php echo Yii::t("default","Start Date")?></label>
+  <?php echo CHtml::textField('start_date1',''  
+  ,array(
+  'class'=>'form-control j_date',
+  'data-id'=>'start_date'
+  ))?>
+</div>
+
+<div class="row">
+  <label class="form-label"><?php echo Yii::t("default","End Date")?></label>
+  <?php echo CHtml::textField('end_date1',''  
+  ,array(
+  'class'=>'form-control j_date' ,
+  'data-id'=>'end_date'
+  ))?>
+</div>
+
+
+<div class="row">
+  <label class="form-label"><?php echo Yii::t("default","Status")?></label>
+  <?php echo CHtml::dropDownList('stats_id[]',array(4),(array)$order_stats,array(
+  'class'=>"chosen form-control",
+  'multiple'=>true
+  ))?>
+</div>
+
+<div class="mt-3">
+  <label class="form-label">&nbsp;</label>
+  <input type="button" class="btn btn-sm btn-success" value="<?php echo t("Search")?>" onclick="sales_summary_reload();">  
+  <a href="javascript:;" rel="rptAdminSalesMerchant" class="btn btn-sm btn-outline-primary"><?php echo t("Export")?></a>
+</div>  
+
+<div style="height:20px;"></div>
+
+<input type="hidden" name="action" id="action" value="rptAdminSalesRpt">
+<input type="hidden" name="tbl" id="tbl" value="item">
+<input type="hidden" name="server_side" id="server_side" value="1">
+<div class="table-responsive">
+<table id="table_list" class="table table-hover table-striped table-condensed datatable-basic">  
+   <thead>
+        <tr> 
+            <th width="2%"><?php echo Yii::t('default',"Ref#")?></th>
+            <th width="6%"><?php echo Yii::t('default',"Merchant Name")?></th>
+            <th width="6%"><?php echo Yii::t('default',"Name")?></th>
+            <th width="6%"><?php echo Yii::t('default',"Contact#")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Item")?></th>            
+            <th width="3%"><?php echo Yii::t('default',"TransType")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Payment Type")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Total")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Tax")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Total W/Tax")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Status")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Platform")?></th>
+            <th width="3%"><?php echo Yii::t('default',"Date")?></th>            
+        </tr>
+    </thead>
+    <tbody>    
+    </tbody>
+</table>
+<div class="clear"></div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
